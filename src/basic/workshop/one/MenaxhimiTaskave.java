61cc2f64ba8c87992c66eed8de4d86b6a286bc36
Naim Sulejmani <naim.sulejmani@keds-energy.com>
2021-04-07 19:17:07 +0200

    workshop 1  - tasks management

--- src/basic/workshop/one/MenaxhimiTaskave.java
+++ src/basic/workshop/one/MenaxhimiTaskave.java
@@ -26,13 +26,13 @@ public class MenaxhimiTaskave {
         //sentinel kur e shkruan EXIT me u nderpre
 
         String task;
-        int taskIndex = 0; //fillo prej zeros
+        int taskCounter = 0; //fillo prej zeros
 
-        System.out.printf("Shkruaj taskun e %d: ", (taskIndex + 1));
+        System.out.printf("Shkruaj taskun e %d: ", (taskCounter + 1));
         task = reader.nextLine();
         //rojtari - SENTINELI qe po kontroll per nje vlere e cila nuk lejohet
-        while (!task.equalsIgnoreCase(SENTINEL) && taskIndex < tasks.length) { // negacioni perdoret me !
-            tasks[taskIndex] = task; // rreshti 35 koment
+        while (!task.equalsIgnoreCase(SENTINEL) && taskCounter < tasks.length) { // negacioni perdoret me !
+            tasks[taskCounter] = task; // rreshti 35 koment
 
             System.out.println("A eshte tasku i perfunduar (P,p per PO, tjerat JO):");
             char perfunduar = reader.nextLine().charAt(0);
@@ -42,18 +42,34 @@ public class MenaxhimiTaskave {
             // TURE || FALSE => TRUE
             // FALSE || TRUE => TRUE
             // FALSE || FALSE => FALSE
-            tasksStatus[taskIndex] = perfunduar == 'P' || perfunduar == 'p';
-            taskIndex++;
+            tasksStatus[taskCounter] = perfunduar == 'P' || perfunduar == 'p';
+            taskCounter++;
             // pre-increment -> ++i, --i
             // post-increment -> i++, i--
-
-
-            System.out.printf("Shkruaj taskun e %d: ", (taskIndex + 1));
+            System.out.printf("Shkruaj taskun e %d: ", (taskCounter + 1));
             task = reader.nextLine();
         }
+        System.out.printf("Te perfunduara: %d%n", countFinishedTasks(tasksStatus, taskCounter));
+        System.out.printf("Te pa-perfunduara: %d%n", (taskCounter - countFinishedTasks(tasksStatus, taskCounter)));
 
+        printAllTask(tasks, tasksStatus, length);
         System.out.println("Aplikacioni u mbyll!!!");
+    }
 
+    public static void printAllTask(String[] tasks, boolean[] tasksStatus, int length) {
+        for (int i = 0; i < length; i++) {
+            // ?: - operatori ternar
+            System.out.printf("%s - %s%n", tasks[i], tasksStatus[i] ? "E KOMPLETUAR" : "E PAPERFUNDUAR");
+        }
+    }
+
+    //metoda e cila pranon vargun me boolean statuse te tasqeve, dhe numeron ato te perfunduara
+    public static int countFinishedTasks(boolean[] tasksStatus, int length) {
+        int counter = 0; //
+        for (int i = 0; i < length; i++) {
+            if (tasksStatus[i]) counter++;
+        }
+        return counter;
     }
 
 
