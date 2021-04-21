1693483127eecdbaa13bb0826d9d4457bff9e62e
naimsulejmani <naim.sulejmani@gmail.com>
2021-04-21 20:31:00 +0200

    instanceof

--- src/oop/ict/kosovo/growth/ushtrimi5_inheritance/upcasting/TestUpcasting.java
+++ src/oop/ict/kosovo/growth/ushtrimi5_inheritance/upcasting/TestUpcasting.java
@@ -1,5 +1,7 @@
 package oop.ict.kosovo.growth.ushtrimi5_inheritance.upcasting;
 
+import oop.ict.kosovo.growth.ushtrimi5_inheritance.BesnikClass;
+
 public class TestUpcasting {
     public static void main(String[] args) {
         Object henry = new FutballPlayer();
@@ -10,7 +12,42 @@ public class TestUpcasting {
         besnikHasi.play();
         besnikHasi.defend();
         ronaldo.play();
-        messi.play();//i thojne polimorfizem
+        messi.play();//i thojne polimorfizem // dynamic binding
+        Object t = new Thread();
+
+
+//        Player p = new Player();
+//        if(p instanceof  String) {
+//
+//        }
+
+
+        //BesnikClass bc = (BesnikClass)henry;
+
+        if(henry instanceof FutballPlayer) {
+            FutballPlayer fp = (FutballPlayer)henry;
+            fp.defend();
+            fp.play();
+            System.out.println("Eshte Football Player");
+        }
+//
+//        if(messi instanceof Player) {
+//            System.out.println("Eshte instance e player");
+//        }
+
+        if(messi instanceof Object) {
+            System.out.println("OBject");
+        }
+        else
+        if(messi instanceof  Player) {
+            System.out.println("Player");
+        }
+        else if(messi instanceof  FutballPlayer) {
+            System.out.println("Futball player");
+        }
+
+
+
 
         //nuk vlene downcasting
         //FutballPlayer player = new Player();
