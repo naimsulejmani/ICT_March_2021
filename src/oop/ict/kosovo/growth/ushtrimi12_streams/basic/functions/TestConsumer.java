6f728ee7f3488188f0a67572e54abcef4d17efa8
naimsulejmani <naim.sulejmani@gmail.com>
2021-05-22 16:45:54 +0200

    Test Consumer

---
+++ src/oop/ict/kosovo/growth/ushtrimi12_streams/basic/functions/TestConsumer.java
@@ -0,0 +1,23 @@
+package oop.ict.kosovo.growth.ushtrimi12_streams.basic.functions;
+
+import oop.ict.kosovo.growth.ushtrimi12_streams.basic.Product;
+
+import java.util.function.Consumer;
+
+public class TestConsumer {
+    public static void main(String[] args) {
+        Consumer<Product> productToConsume = new Consumer<Product>() {
+            @Override
+            public void accept(Product product) {
+                System.out.println("Sa eshte cmimi i produktit");
+                System.out.println(product);
+                System.out.println("A ban pak am lire");
+                product.setDiscount(0.05);
+                System.out.println(product);
+            }
+        };
+
+        productToConsume.accept(new Product(4, "Vaj 1l",2.10));
+
+    }
+}
