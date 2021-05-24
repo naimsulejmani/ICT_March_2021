467fb1ae7cfbc032a799c3357e64358d12cf8366
naimsulejmani <naim.sulejmani@gmail.com>
2021-05-24 19:44:33 +0200

    parallel

--- src/oop/ict/kosovo/growth/ushtrimi12_streams/medium/TestCountMinMaxSumAverage.java
+++ src/oop/ict/kosovo/growth/ushtrimi12_streams/medium/TestCountMinMaxSumAverage.java
@@ -1,6 +1,9 @@
 package oop.ict.kosovo.growth.ushtrimi12_streams.medium;
 
+import java.util.DoubleSummaryStatistics;
+import java.util.IntSummaryStatistics;
 import java.util.OptionalInt;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class TestCountMinMaxSumAverage {
@@ -20,9 +23,15 @@ public class TestCountMinMaxSumAverage {
             System.out.println(max.getAsInt());
 
         int sum = Stream.of(names).distinct().mapToInt((s) -> s.length()).sum();
-
         System.out.println(sum);
-
+        String concat = Stream.of(names).distinct().reduce("", (s1, s2) -> s1 + " " + s2);
+        System.out.println(concat);
+        int a = Stream.of(names).distinct().mapToInt(s -> s.length()).reduce(0, (i, j) -> i + j);
+        System.out.println(a);
+        IntSummaryStatistics statistics = Stream.of(names).parallel().distinct().collect(
+                Collectors.summarizingInt(s->s.length())
+        );
+        System.out.println(statistics);
 
     }
 }
