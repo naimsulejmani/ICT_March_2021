1f45f8eaa1d5fdec4b14a1a2154fa0949ecaf2f4
Naim Sulejmani <naim.sulejmani@gmail.com>
2021-11-02 15:08:03 +0100

    added console colors

---
+++ src/basic/Test.java
@@ -0,0 +1,8 @@
+package basic;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(ConsoleColors.RED + "RED COLORED" +
+                ConsoleColors.RESET + " NORMAL");
+    }
+}
